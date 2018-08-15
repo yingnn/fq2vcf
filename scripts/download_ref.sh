6a5c27f722cc8a2bc702908bf6a671bd27826b05
yn-mac <yingnn@live.cn>
2018-08-15 16:18:44 +0800

    c download_ref

--- scripts/download_ref.sh
+++ scripts/download_ref.sh
@@ -9,7 +9,7 @@ while getopts ":p:" opt
 do
     case $opt in
         p) out_prefix=$OPTARG;;
-        *) echo version must be one of b37 hg19 hg38 && info;;
+        *) echo version must be one of b37 b38 hg19 hg38 && info;;
     esac
 done
 shift $(($OPTIND - 1))
@@ -20,12 +20,15 @@ if [ $# -lt 1 ]; then info; fi
 
 case $1 in
     b37) echo version b37;;
+    b38) echo b38;;
     hg19) echo hg19;;
     hg38) echo hg38;;
-    *) echo version must be one of b37 hg19 hg38 && exit 1;;
+    *) echo version must be one of b37 b38 hg19 hg38 && exit 1;;
 esac
 
 . $var || :
+
+test "$1" = "b38" && wget -c ftp://ftp.ensembl.org/pub/current_fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.toplevel.fa.gz && exit 0
 
 ftp_base=ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/$1
 
